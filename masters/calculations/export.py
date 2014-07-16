421e25b1ff473e093a0b51effe25934e59f3c2ef
gitumarkk <gitumarkk@gmail.com>
2014-07-16 16:42:49 +0200

    fixed excel export bug

--- masters/calculations/export.py
+++ masters/calculations/export.py
@@ -14,31 +14,59 @@ class ExportToExcel(object):
         summary_data = self.data["summary"]
 
         _r = 1
+
         for k, v in summary_data.iteritems():
             _c = 1
             col = get_column_letter(_c)
             ws.cell('%s%s'%(col, _r)).value = k
             _r += 1
             for k_2, v_2 in v.iteritems():
-                col = get_column_letter(_c)
-                ws.cell('%s%s'%(col, _r)).value = k_2
-                ws.cell('%s%s'%(col, _r+1)).value = v_2  # _r + 1 so that data can be in bottom
+                if k_2 == 'initial_metal_conc' or k_2 == 'final_metal_conc':
+                    c_3 = 1
+                    ws.cell('%s%s'%("A", _r)).value = k_2
+                    for k_3, v_3 in v_2.iteritems():
+                        col = get_column_letter(c_3)
+                        ws.cell('%s%s'%(col, _r+1)).value = k_3
+                        ws.cell('%s%s'%(col, _r+2)).value = v_3  # _r + 1 so that data can be in bottom
+                        c_3 += 1
+                    _r += 5
+                else:
+                    col = get_column_letter(_c)
+                    ws.cell('%s%s'%(col, _r)).value = k_2
+                    ws.cell('%s%s'%(col, _r+1)).value = v_2  # _r + 1 so that data can be in bottom
                 _c += 1
             _r += 3  # Leaving a space and accomodating for _r+1 above
 
     def write_general_data(self, ws, data):
         _r = 2
+        map_data_col = {"step": "A"}
+        _char = "B"
         for row in data:
             col = get_column_letter(1)  # Assuming 1st entry is for Time
-            ws.cell('%s%s'%(col, _r)).value = row["step"]
             if _r == 2: # First pas through the loop need to update the header
                 ws.cell('%s%s'%(col, 1)).value = "Time (Min)"
+
+                # BUILD HASH MAPS OF EXCEL COLUMNS TO VALUES AS DICT DOES NOT MAINTAIN POSITION
+                for k in row["ions"].iterkeys():
+                    map_data_col[k] = _char
+                    _char = chr(ord(_char) + 1)
+
+            ws.cell('%s%s'%(col, _r)).value = row["step"]
+
             _c = 2
             for k, v in row["ions"].iteritems():
-                col = get_column_letter(_c) # Reserver first column for step
-                if _r == 2: # First pas through the loop need to update the header
-                    ws.cell('%s%s'%(col, 1)).value = k
-                ws.cell('%s%s'%(col, _r)).value = v
+                # col = get_column_letter(_c) # Reserver first column for step
+                if _r == 2: # First pass through the loop need to update the header
+                    ws.cell('%s%s'%(map_data_col[k], 1)).value = k
+                ws.cell('%s%s'%(map_data_col[k], _r)).value = v
+
+                # ws.cell('%s%s'%(col, 1)).value = k
+                # ws.cell('%s%s'%(col, _r)).value = v
+                # if k == "Sn":
+                #     print _c, ": ",col, ": ",_r, ": ",v, ": ",len(row["ions"])
+            # if _r > 360:
+            #     print row["ions"]
+            #     # print k, v
                 _c += 1
             _r += 1
 
